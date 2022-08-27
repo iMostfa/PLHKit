7b412f20254626988fb8963c4558931716a5afa4
iMostfa <almme111@gmail.com>
2022-08-27 02:29:12 +0200

    Multiple Page Rendering Support.

--- Sources/PLHKit/PDF/PDFPage.swift
+++ Sources/PLHKit/PDF/PDFPage.swift
@@ -9,52 +9,16 @@ import UIKit
 
 public struct PDFPage {
     
-    public enum Paper {
-        case A4
-        case custom(CGFloat, CGFloat)
-        
-        func size() -> CGSize {
-            switch self {
-                
-            case .A4:
-                return .init(width: 595.28 , height: 841.89)
-            case .custom(let width, let height):
-                return .init(width: width, height: height)
-            }
-        }
-    }
+
+    internal var content: SomePLHView
     
-    private let renderer:UIGraphicsPDFRenderer
-    private var content: SomePLHView
-    private var rect: CGRect
+//    public var body: Data {
+//        drawPage()
+//    }
     
-    public var body: Data {
-        drawPage()
-    }
-    
-    public init(_ paper:  Paper = .A4, @PDFUIBuilder _ content: () -> SomePLHView) {
-        let paperSize = paper.size()
-        let rect: CGRect =  .init(origin: .zero, size: .init(width: paperSize.width, height:paperSize.height))
-        self.rect = rect
-        self.renderer = UIGraphicsPDFRenderer(bounds: rect)
+    public init(@PDFUIBuilder _ content: () -> SomePLHView) {
         self.content = content()
         
     }
     
-    private func drawPage() -> Data {
-        let data = renderer.pdfData { context in
-            context.beginPage()
-            let resolvedNodes = content.resolve(context: .init(renderer: context), cachedNode: nil)
-            
-            //MARK: - Drawing Action
-            resolvedNodes.layout(in: .init(renderer: context),
-                                         bounds: .init(rect: rect),
-                                         pass: .init())
-
-        }
-        return data
-    }
-    
-    
-
 }
--- Sources/PLHKit/PDFViewer/PLHViewer.swift
+++ Sources/PLHKit/PDFViewer/PLHViewer.swift
@@ -8,23 +8,23 @@
 import SwiftUI
 
 public struct PLHViewer: View {
-    let PDFPage: PDFPage
+    let pdfFile: PDFFile
   
-    public init(_ creator: () -> PDFPage) {
-        self.PDFPage = creator()
+    public init(_ creator: () -> PDFFile) {
+        self.pdfFile = creator()
     }
     
     public var body: some View {
-        PDFPreviewer(data: PDFPage.body)
+        PDFPreviewer(data: pdfFile.body)
     }
 }
 
 public struct PDFCreatorPreview_Previews: PreviewProvider {
     public static var previews: some SwiftUI.View {
         PLHViewer.init {
-            PDFPage {
-                Text("Hello")
-            }
+            .init(pages: .init({
+                Text("Hello world")
+            }))
         }
     }
 }
